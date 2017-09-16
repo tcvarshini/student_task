a2138d743d8ff37210f00e6da84ae8615b93c8f4
tcvarshini <tcvarshini@gmail.com>
2017-09-16 14:36:03 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main extends Student,StudentArrayOperation,StudentGroup
+ {
+
+	public static void main(String[] args) 
+	{
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
